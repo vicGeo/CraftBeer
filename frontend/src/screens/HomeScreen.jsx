3b845d2cd4148c53a7e9db25103524a6570fdc6a
vicGeo <vicsanig@alumni.upv.es>
2021-04-28 17:36:11 +0200

    added dummy data for testing list of products

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import products from '../products';
+
+const HomeScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeScreen
