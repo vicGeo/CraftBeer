8461c36c5b6d36e2c2dee1650344e2a3fe673967
vicGeo <vicsanig@alumni.upv.es>
2021-04-28 17:42:47 +0200

    created screen HomeScreen.jsx

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import products from '../products';
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import products from "../products";
 
 const HomeScreen = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <h1>Lastest Products</h1>
+      <Row>
+        {products.map((product) => {
+          <Col sm={12} md={6} lg={4}>
+            <h3>{product.name}</h3>
+          </Col>;
+        })}
+      </Row>
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
