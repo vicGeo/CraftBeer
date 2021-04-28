85c20d29631747a1cc1af303999c04e2a86ed71e
vicGeo <vicsanig@alumni.upv.es>
2021-04-28 13:42:30 +0200

    created header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
     <>
+    <Header />
+    <main>
       <h1>Welcome To CraftBeer</h1>
+    </main>
+    <Footer />
     </>
   );
 }
