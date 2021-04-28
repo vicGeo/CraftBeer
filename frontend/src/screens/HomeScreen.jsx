56e90eb89df8284a06b0e48a81c73e90cd4ed2a6
vicGeo <vicsanig@alumni.upv.es>
2021-04-28 19:13:13 +0200

    added Product component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import Product from '../components/Product';
 import products from "../products";
 
 const HomeScreen = () => {
   return (
     <>
-      <h1>Lastest Products</h1>
+      <h1 className="text-center">Latest Products</h1>
       <Row>
-        {products.map((product) => {
-          <Col sm={12} md={6} lg={4}>
-            <h3>{product.name}</h3>
-          </Col>;
-        })}
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={4} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
       </Row>
     </>
   );
