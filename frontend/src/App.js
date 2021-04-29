d0db8ecad550377b37c0052fef944b27f57677a5
vicGeo <vicsanig@alumni.upv.es>
2021-04-29 12:38:17 +0200

    added ProductScreen and change anchor tag by Link from react-router-dom cause we want a SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-        <Route path="/" component={HomeScreen}/>
+        <Route path="/" component={HomeScreen} exact />
+        <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
