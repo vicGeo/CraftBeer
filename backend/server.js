ecab6b3d75b38634a3a6831b3d2fa602f07f1db4
vicGeo <vicsanig@alumni.upv.es>
2021-04-30 20:09:28 +0200

    added custom errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const products = require('./data/products');
+const { notFound } = require('./middleware/errorMiddleware.js');
+const { errorHandler } = require('./middleware/errorMiddleware.js');
 
 const productRoutes = require('./routes/productRoutes');
 
@@ -11,6 +13,11 @@ const app = express();
 
 
 app.use('/api/products', productRoutes);
+
+// Custom error handling
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.get('/', (req, res) => {
     res.send('API is running...');
