8aca1ff86bd76276fd18fcd5abd3290441f0cfb5
vicGeo <vicsanig@alumni.upv.es>
2021-04-30 14:13:39 +0200

    added productSchema and reviewSchema

---
+++ backend/models/productModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true},
+    rating: { type: Number, required: true},
+    comment: { type: String, required: true}
+}, {
+    timestamps: true
+});
+
+
+const productSchema = mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    brand: {
+        type: String,
+        require: true
+    },
+    category: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    Price: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    countInStock: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model('Produc', productSchema);
+
+module.exports= Product;
