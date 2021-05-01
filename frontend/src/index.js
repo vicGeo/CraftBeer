bd59e526e59a585ec8ea53141d4921026944e061
vicGeo <vicsanig@alumni.upv.es>
2021-05-01 10:59:01 +0200

    added Provider in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap.min.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
+  <Provider store={store}>
     <App />,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
