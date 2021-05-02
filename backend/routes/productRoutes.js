20e0857e14810e28ea76c55dd1f67bb65e0b1edc
vicGeo <vicsanig@alumni.upv.es>
2021-05-02 11:33:00 +0200

    added productController to clean up productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const Product = require('../models/productModel');
-
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+const { getProducts, getProductById } = require('../controllers/productController');
 
 
-    res.json(products);
-}));
+router.route('/').get(getProducts);
 
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @access Public
 
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
 
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}));
+router.route('/:id').get(getProductById);
 
 
 
