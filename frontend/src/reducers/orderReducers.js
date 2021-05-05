822f99346efacda124cf432f809f6e5e2c52fd65
vicGeo <vicsanig@alumni.upv.es>
2021-05-05 13:39:11 +0200

    debugged logout action -  clean detailsUser and reset orderList

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,7 +11,8 @@ import {
   ORDER_PAY_RESET,
   ORDER_LIST_MY_SUCCESS,
   ORDER_LIST_MY_REQUEST,
-  ORDER_LIST_MY_FAIL
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -36,7 +37,10 @@ export const orderCreateReducer = (state = {}, action) => {
   }
 };
 
-export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {}}, action) => {
+export const orderDetailsReducer = (
+  state = { loading: true, orderItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case ORDER_DETAILS_REQUEST:
       return {
@@ -58,7 +62,7 @@ export const orderDetailsReducer = (state = { loading: true, orderItems: [], shi
   }
 };
 
-export const orderPayReducer = (state = { }, action) => {
+export const orderPayReducer = (state = {}, action) => {
   switch (action.type) {
     case ORDER_PAY_REQUEST:
       return {
@@ -75,7 +79,7 @@ export const orderPayReducer = (state = { }, action) => {
         error: action.payload,
       };
     case ORDER_PAY_RESET:
-      return {}
+      return {};
     default:
       return state;
   }
@@ -96,6 +100,10 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
       return {
         loading: false,
         error: action.payload,
+      };
+    case ORDER_LIST_MY_RESET:
+      return {
+        orders: [],
       };
     default:
       return state;
