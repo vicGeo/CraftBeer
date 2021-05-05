e6077c74c62856b9ea64e6663717f0d007fe07f8
vicGeo <vicsanig@alumni.upv.es>
2021-05-05 11:56:42 +0200

    added PayPal button in OrderScreen and logical to show sandbox paypal account

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
-// Access to Paypal cliente ID
+// Access to Paypal client ID
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 // Custom error handling
