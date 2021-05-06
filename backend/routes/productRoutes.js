f1150f1bf60cd426150aa7593f1a3a2577a9665f
vicGeo <vicsanig@alumni.upv.es>
2021-05-06 12:31:25 +0200

    added functionality to delete products in productController backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductById } = require('../controllers/productController');
+const { getProducts, getProductById, deleteProduct } = require('../controllers/productController');
+const protect = require("../middleware/authMiddleware");
+const admin = require("../middleware/adminMiddleware");
 
 
 router.route('/').get(getProducts);
 
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 
 
