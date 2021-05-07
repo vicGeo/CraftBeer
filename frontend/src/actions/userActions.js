4d1b036b916a4bea021bb5a1f4e353de8fffd9cf
vicGeo <vicsanig@alumni.upv.es>
2021-05-07 10:41:57 +0200

    debugged userAction logout. Now removeItem shippingAdress from localstorage

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -65,6 +65,9 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
+  localStorage.removeItem('cartItems')
+  localStorage.removeItem('shippingAddress')
+  localStorage.removeItem('paymentMethod')
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
