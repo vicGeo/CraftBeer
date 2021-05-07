92463b665472198a81999e308c31bc83753e7432
vicGeo <vicsanig@alumni.upv.es>
2021-05-07 17:43:59 +0200

    installed multer-s3 and aws-sdk for upload images to persistence in heroku

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,43 +1,50 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const path = require("path");
 
-// From documentation of multer
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
+
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+
+
+const s3 = new aws.S3();
+aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  accessSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "eu-west-1",
 });
 
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
   } else {
-    cb("Images only!");
+    cb(new Error("Invalid file type, only JPEG and PNG is allowed!"), false);
   }
-}
+};
 
 const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
+  fileFilter,
+  storage: multerS3({
+    acl: "public-read",
+    s3: s3,
+    bucket: `craftbeergeekshubs`,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: "TESTING_METADATA" });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
 });
 
+const returnImageUrl = (req, res) => {
+  return res.send(`${req.file.location}`);
+};
 
-router.post("/", upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path.replace('\\', '/')}`);
-});
+
+router
+  .route("/")
+  .post(upload.single("image"), returnImageUrl);
+
 
 module.exports = router;
