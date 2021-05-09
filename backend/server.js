5d750fef384ca86e18e00957ea8be73f2bf0bcea
vicGeo <vicsanig@alumni.upv.es>
2021-05-09 14:20:40 +0200

    making changes to deploy on heroku. Changed environment to production

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { notFound } = require("./middleware/errorMiddleware.js");
 const { errorHandler } = require("./middleware/errorMiddleware.js");
 const cors = require("cors");
 const path = require("path");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 // Routes
 const productRoutes = require("./routes/productRoutes.js");
@@ -18,8 +18,8 @@ dotenv.config();
 connectDB();
 const app = express();
 
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(cors());
@@ -30,23 +30,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-// Access to Paypal client ID
-app.get("/api/config/paypal", (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
-
-// Custom error handling
-app.use(notFound);
-app.use(errorHandler);
-
-// Turn on folder uploads static
-const folder = path.resolve();
-app.use('/uploads', express.static(path.join(folder, '/uploads')));
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
@@ -55,6 +38,29 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+
+// Access to Paypal client ID
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+// Turn on folder uploads static
+const folder = path.resolve();
+app.use("/uploads", express.static(path.join(folder, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(folder, "/frontend/build")));
+
+  app.get("*", (req, res) =>  res.sendFile(path.resolve(folder, 'frontend', 'build', 'index.html')));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// Custom error handling
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
